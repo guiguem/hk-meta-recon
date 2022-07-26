8ff2c8ea95164077da6d233224b9336e1912fb54
Mathieu Guigue <mgguigue@gmail.com>
2022-07-26 17:21:58 +0200

    First implementation with ROOT and G4

---
+++ hkinstall.py
@@ -0,0 +1,17 @@
+from hkpilot.utils.metapackage import MetaPackage
+
+import inspect
+import os
+
+
+class ExternalsMeta(MetaPackage):
+
+    def __init__(self, path):
+        super().__init__(path)
+
+        self._package_name = "hk-meta-externals"
+        self._package_version = "v0.0.1"
+        self._depends_on = {
+            "ROOT": "*",
+            "Geant4": "*"
+            }
