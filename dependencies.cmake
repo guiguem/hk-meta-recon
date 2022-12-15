abc3c61201c3d069e7106d1d0c5df92b2a28cc72
Mathieu Guigue <mgguigue@gmail.com>
2022-12-15 13:54:25 +0100

    Update all externals versions

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,4 +1,4 @@
-hk_package( ToolFrameworkCore *)
-hk_package( ROOT *)
-hk_package( WCSim *)
-hk_package( Geant4 *)
+hk_package( ToolFrameworkCore 1.0.0)
+hk_package( ROOT 6.26.04)
+hk_package( WCSim 1.10.1)
+hk_package( Geant4 10.1.3)
